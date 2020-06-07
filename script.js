9bb9fe9d7d7da927e06b3302769941131a729bd2
red-wisp <59499834+red-wisp@users.noreply.github.com>
2020-06-07 23:11:23 +0400

    кнопки работают

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ let form1 = document.getElementById('main_form');
 let form2 = document.getElementById('main_form2');
 let form3 = document.getElementById('main_form3');
 
-form2.se
+
+let add_bt_1 = document.getElementById('bt_1');
+let add_bt_2 = document.getElementById('bt_2');
+
+
+form2.classList.add('el_none');
+form3.classList.add('el_none');
 console.log(form1);
 
+add_bt_1.onclick = function() {
+    form2.classList.remove('el_none');
+    add_bt_1.classList.add('el_none');
+  };
+  
+add_bt_2.onclick = function() {
+    form3.classList.remove('el_none');
+    add_bt_2.classList.add('el_none');
+  };
