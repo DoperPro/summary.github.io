84d38dedefd35c6fcbda8745a111730cbcca8beb
red-wisp <59499834+red-wisp@users.noreply.github.com>
2020-06-07 23:00:12 +0400

    кнопки

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let form1 = document.getElementById('main_form');
 let form2 = document.getElementById('main_form2');
 let form3 = document.getElementById('main_form3');
 
-
+form2.se
 console.log(form1);
 
